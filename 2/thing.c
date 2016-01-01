15a980341b525d9690a68b46a7856208011e17f8
Drahman AbdulBasir <durmon.albasir@gmail.com>
2015-12-31 22:58:03 -0500

    ignore things. makefile things

--- 2/thing.c
+++ 2/thing.c
@@ -7,6 +7,9 @@ int main ()
 
 	printf("I am %d years old.\n", age);
 	printf("I am %d inches tall.\n", height);
+	printf("I am an elephant\n");
+	printf("I am a banana\n");
+	printf("You are?\n");
 
 	return 0;
 }
